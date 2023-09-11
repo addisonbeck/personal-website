ff7cec1224a59b999d5c047ff1233ce3f096150c
addison <addisonbeck1@gmail.com>
2023-09-11 19:45:09 -0400

    Update email on contact page

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
   import './contact.css';
+	import { onMount } from 'svelte'
+  let message = 'hi';
+  const messages = [
+    'hi',
+    'hello',
+    'howdy',
+    '您好',
+    'salut',
+    'gutentag',
+    'السلام عليكم',
+    '0100100001101001',
+    'oi',
+    'こんにちは',
+    'please-work-here',
+    'or-pretty-much-anything',
+  ];
+
+  onMount(() => {
+		setInterval(() => {
+      message = messages[messages.indexOf(message) + 1]  ?? messages[0];
+		}, 1500);
+  });
 </script>
 
 <div class="contact-container">
   <div class="contact-card">
-    Shoot me an email at <a href="mailto:addisonbeck1@gmail.com">addisonbeck1@gmail.com</a>
+    <span>Shoot me an email at </span>
+    <a href='mailto:${message}@addisonbeck.com'>
+      {message}@addisonbeck.com
+    </a>
   </div>
 </div>
 
